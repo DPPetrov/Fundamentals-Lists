468633b677e5e4e6df26aa693367ae75b51542cc
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-06-10 21:11:33 +0300

    adjusted output to match same row display req

--- ReverseNumbers.java
+++ ReverseNumbers.java
@@ -14,7 +14,7 @@ public class ReverseNumbers {
 
         for (int i = numbersToReverse.length; i > 0 ; i--) {
 
-            System.out.println(numbersToReverse[i - 1]);
+            System.out.print(numbersToReverse[i - 1] + " ");
         }
 
     }
