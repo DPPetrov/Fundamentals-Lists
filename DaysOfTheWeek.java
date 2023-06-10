359407b31e6478000c40fa88f764fe1dd1fd998d
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-06-10 20:14:15 +0300

    added days array and scanner

---
+++ DaysOfTheWeek.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DaysOfTheWeek {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int day = Integer.parseInt(sc.nextLine());
+
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
+        , "Saturday", "Sunday"};
+
+        for (int i = 0; i < days.length; i++) {
+
+        }
+    }
+}
