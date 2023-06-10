a3668a0ef98ba3b93aca30d19ab63103f19dcb09
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-06-10 21:02:49 +0300

    added user input and logic to fill array

---
+++ ReverseNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ReverseNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfLines = Integer.parseInt(sc.nextLine());
+        int[] numbersToReverse = new int[numberOfLines];
+
+        for (int i = 0; i < numbersToReverse.length; i++) {
+
+            int userInput = Integer.parseInt(sc.nextLine());
+            numbersToReverse[i] =  userInput;
+        }
+
+
+    }
+}
